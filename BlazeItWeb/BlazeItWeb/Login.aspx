1ae5fc651acb6de5e195b1f069b8d9c300bd2edc
dragoonred <coreypeers@hotmail.com>
2018-04-03 18:25:18 -0400

    Added in some base stuff. Added a login page with some comments. Added web config data for db connection strings.

---
+++ BlazeItWeb/BlazeItWeb/Login.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="BlazeItWeb.WebForm1" %>
+
+<!DOCTYPE html>
+<!-- This page needs a simple form for log in information. accountname password, and a submit button. other stuff to make it 'look cool' maybe. -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BlazeItWeb/BlazeItWeb/Login.aspx.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * This File is the backend for the login page. It will handle the user input for their username and password
+ * Which will then connect and verify against the database. Depending on the result they will be logged into the application
+ * or be denied, if they are logged in the webpage will be loaded based on their account specifications.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BlazeItWeb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
